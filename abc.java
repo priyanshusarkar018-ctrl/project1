846f2cbc74e8ac4da8409b25a1662cb44893be1c
Priyanshu <priyanshusarkar018@gmail.com>
2025-11-20 22:59:15 +0530

    modified java file is added

---
+++ abc.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class abc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter input: ");
+        String input = sc.nextLine();
+        System.out.println("You entered: " + input);
+        sc.close();
+    }
+}
